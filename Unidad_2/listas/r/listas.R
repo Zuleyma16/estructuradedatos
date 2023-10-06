8dae56d76fefb5488e4769f3d3612d30a3bb0d46
unknown <zuleymadejesusmanzano@gmail.com>
2023-10-06 10:11:32 -0600

    mi primer commit

---
+++ Unidad_2/listas/r/listas.R
@@ -0,0 +1,32 @@
+> ITIC3 <- list("Fer Pantoja", "Emanuel Esparza", "Xitlaly Gonzales");
+> Edades <- list(18,19,21);
+> Crreras <- list("ITIC", "Logistica", "Gestion E", "Mecatronica", "Industrial");
+> EdadesAlumnos <- list(ITIC3, Edades);
+> View(EdadesAlumnos)
+> View(Edades)
+> ITIC3[3];
+> EdadesAlumnos[1];
+> EdadesAlumnos[[1]][[1]];
+> View(Edades)
+> length(Edades)
+> length(EdadesAlumnos)
+> Edades[4] <- ("32");
+> Edades <- ("30");
+> Edades
+> Edades <- list(18,19,21,30);
+> Edades[5] <- list(25);
+> View(Edades)
+> View(Edades)
+> Edades
+> Edades[4] <- NULL;
+> View(Edades)
+> Edades
+> ITIC3[5] <- list("Mely")
+> View(ITIC3)
+> ITIC3
+> ITIC3[1] <- NULL
+> View(ITIC3)
+> ITIC3
+> Edades2 <- list(17,20,21);
+> View(Edades2)
+> Edades2
